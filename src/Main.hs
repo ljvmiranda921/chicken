dd75c38cce68c69c55d1da17a8943fbeda981766
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-03-30 20:33:39 +0800

    Add parseCheckins main function

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,48 @@
 module Main where
 
+import           Text.CSV
+import           Text.Printf
+import           Data.List.Split
+import           Options.Applicative
+
+
+data App = App { appPath :: String }
+
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = execParser opts >>= runWithOptions
+ where
+  parser = App <$> argument str (metavar "PATH")
+  opts   = info parser mempty
+
+runWithOptions :: App -> IO ()
+runWithOptions opts = parseCheckins (appPath opts)
+
+
+-- | The `parseCheckins` function takes a filepath and prints out the formatted
+-- checkin from Toggl
+parseCheckins f = do
+  csv_file <- parseCSVFromFile f
+  putStrLn "checkin"
+  case csv_file of
+    Right csv -> mapM_ putStrLn (parseCheckins' (tail csv))
+    Left  err -> print err
+parseCheckins' csv = [ format record | record <- csv, record /= [""] ]
+
+-- | The `format` function stylizes the Toggle entry so that it looks just
+-- like your #dailycheckin log.
+format record = printf "- %0.2f hrs #%s %s" duration project entry
+ where
+  project  = (record !! 0)
+  entry    = (record !! 2)
+  duration = getDuration (record !! 3)
+
+-- | The `getDuration` function first splits the time string, then applies the
+-- getDuration' function to compute for the total elapsed time.
+getDuration :: String -> Double
+getDuration s = getDuration' time where time = map read (splitOn ":" s)
+
+-- | The `getDuration'` function gets the hour-based representation of the work
+-- duration, making it #dailycheckin-compatible.
+getDuration' :: [Double] -> Double
+getDuration' xs = sum [ (t / 60 ^ i) | (i, t) <- l ] where l = zip [0 ..] xs
